8f3a585feec14a281ac885c3ea0d55273208715f
Peter Denisevich <denisevich.p@gmail.com>
2014-06-19 00:52:28 +0300

    changed formatting and some other stuff

--- p_d_first/test_search.py
+++ p_d_first/test_search.py
@@ -1,10 +1,8 @@
 # coding: utf-8
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
-import unittest, time, re
+import unittest
+
 
 class Untitled(unittest.TestCase):
     def setUp(self):
@@ -26,25 +24,30 @@ class Untitled(unittest.TestCase):
         driver.find_element_by_link_text("ptluser2").click()
     
     def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException, e: return False
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException, e:
+            return False
         return True
     
     def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException, e: return False
+        try:
+            self.driver.switch_to.alert
+        except NoAlertPresentException, e:
+            return False
         return True
     
     def close_alert_and_get_its_text(self):
         try:
-            alert = self.driver.switch_to_alert()
+            alert = self.driver.switch_to.alert
             alert_text = alert.text
             if self.accept_next_alert:
                 alert.accept()
             else:
                 alert.dismiss()
             return alert_text
-        finally: self.accept_next_alert = True
+        finally:
+            self.accept_next_alert = True
     
     def tearDown(self):
         self.driver.quit()
