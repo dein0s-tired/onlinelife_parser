f5a6cd174b4e0c70ac879ed341084f2cf608e1aa
Peter Denisevich <denisevich.p@gmail.com>
2014-06-19 01:11:07 +0300

    deleted unused and unnecessary strings

--- p_d_first/test_search.py
+++ p_d_first/test_search.py
@@ -1,10 +1,9 @@
 # coding: utf-8
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
 import unittest
 
 
-class Untitled(unittest.TestCase):
+class WorldOfTanks_RU(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
@@ -12,7 +11,7 @@ class Untitled(unittest.TestCase):
         self.verificationErrors = []
         self.accept_next_alert = True
     
-    def test_untitled(self):
+    def test_SearchUser(self):
         driver = self.driver
         driver.get(self.base_url + "/")
         driver.find_element_by_xpath("//li[6]/span").click()
@@ -22,32 +21,6 @@ class Untitled(unittest.TestCase):
         driver.find_element_by_id("account_table_search").send_keys("ptluser2")
         driver.find_element_by_id("account-table-search-button").click()
         driver.find_element_by_link_text("ptluser2").click()
-    
-    def is_element_present(self, how, what):
-        try:
-            self.driver.find_element(by=how, value=what)
-        except NoSuchElementException, e:
-            return False
-        return True
-    
-    def is_alert_present(self):
-        try:
-            self.driver.switch_to.alert
-        except NoAlertPresentException, e:
-            return False
-        return True
-    
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to.alert
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally:
-            self.accept_next_alert = True
     
     def tearDown(self):
         self.driver.quit()
