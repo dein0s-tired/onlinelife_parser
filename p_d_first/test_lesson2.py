a21b0d5ca0d8ccf0a9187d23e4126173d7a854c3
Peter Denisevich <denisevich.p@gmail.com>
2014-06-21 01:24:06 +0300

    lesson 2

--- p_d_first/test_lesson2.py
+++ p_d_first/test_lesson2.py
@@ -1,7 +1,4 @@
-__author__ = 'dein0s'
-
 # coding: utf-8
-
 from selenium import webdriver
 import unittest
 
@@ -10,16 +7,15 @@ class Selenium2ru(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
-        self.baseURL ='http://selenium2.ru'
+        self.base_url ='http://selenium2.ru'
         self.verificationErrors = []
-        self.accept_next_allert = True
 
     def test_clickDocuments(self):
         driver = self.driver
-        driver.get(self.baseURL + '/')
+        driver.get(self.base_url + '/')
         driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div[2]/ul/li[2]/a').click()
 
-    def end_of_test(self):
+    def tearDown(self):
         self.driver.quit()
 
 if __name__ == "__main__":
