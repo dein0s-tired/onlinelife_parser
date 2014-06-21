e3047c3d6353681431669d5fbc9a0c504379282d
Peter Denisevich <denisevich.p@gmail.com>
2014-06-21 10:12:58 +0300

    added one more search

--- p_d_first/test_search.py
+++ p_d_first/test_search.py
@@ -7,22 +7,28 @@ class WorldOfTanks_RU(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
-        self.base_url = "http://worldoftanks.ru"
+        self.base_url = 'http://worldoftanks.ru'
         self.verificationErrors = []
         self.accept_next_alert = True
     
-    def test_SearchUser(self):
+    def test_searchPlayer(self):
         driver = self.driver
         #todo: define steps
-        driver.get(self.base_url + "/")
+        #/community/accounts/
+        driver.get(self.base_url + '/')
         driver.find_element_by_xpath("//li[6]/span").click()
-        driver.find_element_by_link_text(u"Игроки").click()
-        driver.find_element_by_id("account_table_search").click()
-        driver.find_element_by_id("account_table_search").clear()
-        driver.find_element_by_id("account_table_search").send_keys("ptluser2")
-        driver.find_element_by_id("account-table-search-button").click()
-        driver.find_element_by_link_text("ptluser2").click()
-    
+        driver.find_element_by_xpath("//a[@href='/community/accounts/']").click()
+        driver.find_element_by_id('account_table_search').click()
+        driver.find_element_by_id('account_table_search').send_keys('ptluser2')
+        driver.find_element_by_id('account-table-search-button').click()
+        driver.find_element_by_link_text('ptluser2').click()
+        #/community/
+        driver.get(self.base_url + '/')
+        driver.find_element_by_xpath("//a[@href='/community/']").click()
+        driver.find_element_by_id('accounts_table_search').click()
+        driver.find_element_by_id('accounts_table_search').send_keys('ptluser2')
+        driver.find_element_by_id('account-table-search-button').click()
+
     def tearDown(self):
         self.driver.quit()
         self.assertEqual([], self.verificationErrors)
