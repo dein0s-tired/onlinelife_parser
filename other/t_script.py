cbc76b3bc492b3a0b0b2794a286a8c44a555a483
Peter Denisevich <denisevich.p@gmail.com>
2014-07-31 22:30:25 +0300

    doesnt care

--- other/t_script.py
+++ other/t_script.py
@@ -9,16 +9,14 @@ updated = '29.07.2014'
 
 def create_parser():
     parser = argparse.ArgumentParser(
-        description=u'This script is used to test Forum API through XMLRPC-channel and only for methods allowed to [NC]'
-                    u' aka notifier. If you getting "Wrong_api_key" - you suck!',
+        description='Nothing to add here',
         add_help=True,
     )
     #subparsers = parser.add_subparsers(dest='command')
     # method_parser = subparsers.add_parser('')
     # parser.add_argument('name', nargs='?', default='World')
-    parser.add_argument('-m', '--method', required=True, help='List of available methos: postNotification, postTopic,'
-                                                              'postPersonalMessage')
-    parser.add_argument('-r', '--realm', required=True, help='List of available realms: WGS1, WGT1')
+    parser.add_argument('-m', '--method', required=True, help='Stub')
+    parser.add_argument('-r', '--realm', required=True, help='Stub')
     parser.add_argument('-u', '--updated', action='version', help='Script last update date',
                         version='%s last time updated %s' % (sys.argv[0], updated))
     return parser
@@ -27,7 +25,7 @@ def create_parser():
 if __name__ == "__main__":
     parser = create_parser()
     namespace = parser.parse_args(sys.argv[1:])
-    if namespace.realm not in ('WGS1', 'WGT1'):
+    if namespace.realm not in ('STUB1', 'STUB2'):
         namespace.realm = 'You SUCK'
 
     print namespace
